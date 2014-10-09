c962db44ef6b0d7ebe3c3c97e8f94fc4a220bd49
Arman Khalatyan <arm2arm@gmail.com>
2014-10-09 12:53:29 +0200

    parallel copy using rsync,parallel,lfs

---
+++ lib/copy-dir-in-parallel.sh
@@ -0,0 +1,52 @@
+#this script generates file names first then copy this in parallel
+#usage:
+#1) copy-dir-in-parallel.sh /lustre/arm2arm backup241:/lustre
+#1) copy-dir-in-parallel.sh /lustre/arm2arm backup241:/lustre lfs
+lustre=lfs
+lustre=$3
+mode=$4
+procs=$5
+src=$1
+dst=$2
+name=$(echo "$src" | tr / -)
+dname=$(echo "$dst" | tr / -| tr : -)
+time=$(date +"%Y%m%d-%H%M")
+logdir=/lustre/tmp/var/logs/copy-dir-in-parallel
+logfile=$logdir/${time}${name}-${dname}.log
+folder=$(basename $src)
+
+workdir=/lustre/tmp/psync/parallel-sync-${time}-$folder
+jobqueuefile=/lustre/tmp/var/log/jobqueue.parallel.$folder
+mkdir -p $logdir
+mkdir -p $workdir
+
+touch  $logfile
+
+cd $workdir
+
+dirs=$workdir/dirs.log
+files=$workdir/files.log
+
+#rsync --log-file=${logfile} -au /glu/$1 /lustre
+
+cd $src && $lustre find . -type d > ${dirs} 
+cd $src && $lustre find . -type f > ${files}
+nfiles=$(wc -l ${files}| cut -d ' ' -f1 )
+ndirs=$(wc -l ${dirs}  | cut -d ' ' -f1)
+
+echo "Found files:$nfiles in $ndirs folders."
+cd $workdir
+split -l 10000 -d -a 6 ${files} files_
+truncate -s 0 $jobqueuefile
+
+for i in files_??????
+do
+truncate -s 0 ${workdir}/${i}.log 
+echo "rsync  -aH --no-l  -e 'ssh -c arcfour '  --files-from=$workdir/${i} --log-file=${workdir}/${i}.log $src $dst/${folder}/ && bzip2 ${workdir}/${i}.log" >> $jobqueuefile 
+done 
+
+pwd 
+echo '#queue is ready, please run, you cah edit line for remote servers as well:  parallel -P4 -Slei1,lei2,lei3'
+echo "cat $jobqueuefile | parallel -P4 --progress "
+cat $jobqueuefile | parallel   -P8  
+
